f2bd3022397b490b70898519c8c57a2f814177da
vaibha987 <v2goyal95@gmail.com>
2024-09-22 16:59:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,18 +29,6 @@ class Teacher extends Person {
 		console.log(`${this.name} is studying`)
 	}
 }
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
-
-person.age = 30;
-console.log(person.age); // Output: 30
-
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
-
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
